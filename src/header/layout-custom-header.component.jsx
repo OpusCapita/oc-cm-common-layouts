00b7d7800c63cc6d28fcda6f4f4b2e419a2f50c7
Matkala Jani <Jani.Matkala@opuscapita.com>
2018-05-30 18:09:22 +0300

    finalize custom header component

--- src/header/layout-custom-header.component.jsx
+++ src/header/layout-custom-header.component.jsx
@@ -1,4 +1,3 @@
-import styled from 'styled-components';
 import React from 'react';
 import PropTypes from 'prop-types';
 // App imports
@@ -6,31 +5,18 @@ import { classPrefix } from '../constants';
 import HeaderPrimitive from '../primitives/layout-primitive-header.component';
 
 const Header = HeaderPrimitive.extend`
-  flex: 1;
 `;
 
-
-const HeaderSection = styled.section`
-color:red;
-`;
-const LayoutHeader = ({ left, center, right }) => (
-  <Header className={`${classPrefix}_header`}>
-    <HeaderSection className={`${classPrefix}_header_section left`}>{left}</HeaderSection>
-    <HeaderSection className={`${classPrefix}_header_section center`}>{center}</HeaderSection>
-    <HeaderSection className={`${classPrefix}_header_section right`}>{right}</HeaderSection>
-  </Header>
+const LayoutCustomHeader = ({ children }) => (
+  <Header className={`${classPrefix}_header`}>{children}</Header>
 );
 
-LayoutHeader.propTypes = {
-  left: PropTypes.node,
-  center: PropTypes.node,
-  right: PropTypes.node,
+LayoutCustomHeader.propTypes = {
+  children: PropTypes.node,
 };
 
-LayoutHeader.defaultProps = {
-  left: undefined,
-  center: undefined,
-  right: undefined,
+LayoutCustomHeader.defaultProps = {
+  children: null,
 };
 
-export default LayoutHeader;
+export default LayoutCustomHeader;
