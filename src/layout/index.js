55c6bf1dfd95d2b83433ada952d456768b323ed7
Matkala Jani <Jani.Matkala@opuscapita.com>
2018-05-30 17:57:18 +0300

    update .gitignore, remove unneeded files

--- src/layout/index.js
+++ src/layout/index.js
@@ -3,17 +3,24 @@ import Column from './columns/layout-column.component';
 import Row from './columns/layout-row.component';
 import Card from './card/layout-card.component';
 import Masonry from './masonry/layout-masonry.component';
-import Header from './header/layout-header.component';
+import BasicHeader from './header/layout-header.component';
 import CustomHeader from './header/layout-custom-header.component';
 import NavigationHeader from './header/layout-navigation-header.component';
 
 export const Layout = {
   Wrapper,
+};
+
+export const Content = {
   Column,
   Row,
   Card,
   Masonry,
-  Header,
-  CustomHeader,
-  NavigationHeader,
+};
+
+
+export const Header = {
+  Basic: BasicHeader,
+  Custom: CustomHeader,
+  Navigation: NavigationHeader,
 };
