e2119e1b04ca55ca64cce529674f45c1f9150cae
Jenni Ristonmaa <jenni.ristonmaa@opuscapita.com>
2018-08-27 07:57:54 +0300

    Removed layout width to fit in with sidebar

--- src/layout/layout-wrapper.component.jsx
+++ src/layout/layout-wrapper.component.jsx
@@ -5,7 +5,6 @@ import styled from 'styled-components';
 import { classPrefix } from '../constants';
 
 const StyledWrapper = styled.div`
-  width:100vw;
   height:100vh;
 `;
 const Wrapper = ({ children, id, className }) => (
