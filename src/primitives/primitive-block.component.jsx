c51c85e22dc376bbca3d28a2f68c1e36160cbe2f
Matkala Jani <Jani.Matkala@opuscapita.com>
2018-09-12 15:48:06 +0300

    clean up unused variables

--- src/primitives/primitive-block.component.jsx
+++ src/primitives/primitive-block.component.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { theme } from '../constants';
 
 const PrimitiveBlock = styled.div`
-  background-color: ${theme.card.backgroundColor};
+  background-color: ${theme.contentBackgroundColor};
   height: 100%;
   width: 100%;
   padding: ${theme.gutterWidth};
