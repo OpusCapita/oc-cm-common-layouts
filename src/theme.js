20568d6dfc13c1e18320d755970d253e6b780d88
Matkala Jani <Jani.Matkala@opuscapita.com>
2018-09-20 12:09:33 +0300

    fixed theme. oops.

--- src/theme.js
+++ src/theme.js
@@ -20,54 +20,55 @@ export default {
     spinner: {
       height: '2rem',
     },
-    column: {
-      minWidth: '400px',
+  },
+  column: {
+    minWidth: '400px',
+  },
+  inputRow: {
+    labelWidth: '225px',
+    labelContainerLineHeight: '3rem',
+    errorContainer: {
+      height: '1.5rem',
     },
-    inputRow: {
-      labelWidth: '225px',
-      labelContainerLineHeight: '3rem',
-      errorContainer: {
-        height: '1.5rem',
-      },
-      requiredIndicator: {
-        fontSize: vars.fontSizeLarge,
-      },
+    requiredIndicator: {
+      fontSize: vars.fontSizeLarge,
     },
-    inputColumn: {
-      labelWidth: '225px',
-      labelContainerLineHeight: '1.5rem',
-      errorContainer: {
-        height: '1.5rem',
-      },
-      requiredIndicator: {
-        fontSize: vars.fontSizeLarge,
-      },
+  },
+  inputColumn: {
+    labelWidth: '225px',
+    labelContainerLineHeight: '1.5rem',
+    errorContainer: {
+      height: '1.5rem',
     },
-    text: {
-      title: {
-        fontSize: vars.fontSizeMedium,
-        fontWeight: vars.fontWeightBold,
-      },
-      subTitle: {
-        fontSize: vars.fontSizeMedium,
-        fontWeight: vars.fontWeightNormal,
-      },
-      error: {
-        fontSize: vars.fontSizeSmall,
-        color: colors.colorError,
-      },
+    requiredIndicator: {
+      fontSize: vars.fontSizeLarge,
     },
-    formControls: {
-      input: {
-        borderColor: colors.colorGray,
-        focusBorderColor: colors.colorYellow,
-        height: '34px',
-      },
-      button: {
-        backgroundColor: colors.colorButtonNormal,
-        primaryBackgroundColor: colors.colorButtonPriorityNormal,
-        textColor: colors.colorWhite,
-        minWidth: '70px',
-      },
+  },
+  text: {
+    title: {
+      fontSize: vars.fontSizeMedium,
+      fontWeight: vars.fontWeightBold,
     },
-  };
+    subTitle: {
+      fontSize: vars.fontSizeMedium,
+      fontWeight: vars.fontWeightNormal,
+    },
+    error: {
+      fontSize: vars.fontSizeSmall,
+      color: colors.colorError,
+    },
+  },
+  formControls: {
+    input: {
+      borderColor: colors.colorGray,
+      focusBorderColor: colors.colorYellow,
+      height: '34px',
+    },
+    button: {
+      backgroundColor: colors.colorButtonNormal,
+      primaryBackgroundColor: colors.colorButtonPriorityNormal,
+      textColor: colors.colorWhite,
+      minWidth: '70px',
+    },
+  },
+};
